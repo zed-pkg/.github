344a4b319a1176212829b9468515765aaa808c55
Alexander Mills <alex@oresoftware.com>
2026-08-05 16:14:03 -0500

    fix Visual Studio candidate imports

--- blueprints/zed-visual-studio/src/Zed.VisualStudio.Core/ZedInspector.cs
+++ blueprints/zed-visual-studio/src/Zed.VisualStudio.Core/ZedInspector.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Zed.VisualStudio.Core;
 
